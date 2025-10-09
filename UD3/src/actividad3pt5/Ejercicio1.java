929170a23e4c0dd0f610669f7c7120934921a00d
DNG222-code <dnagy20474@iesjoanramis.org>
2025-10-09 21:24:37 +0200

    actividad3pt5.

---
+++ UD3/src/actividad3pt5/Ejercicio1.java
@@ -0,0 +1,12 @@
+package actividad3pt5;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        // Declaramos las variables para poder calcular las medidas.
+        int ladoCorto;
+        int ladoLargo;
+
+
+    }
+
+}
