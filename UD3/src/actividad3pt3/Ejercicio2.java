8cd78fe10651da2e05cc4e134e022fed9b8e493b
DNG222-code <dnagy20474@iesjoanramis.org>
2025-10-09 20:52:48 +0200

    Modificar el Ejercicio2 del package actividad3pt3.

--- UD3/src/actividad3pt3/Ejercicio2.java
+++ UD3/src/actividad3pt3/Ejercicio2.java
@@ -2,8 +2,18 @@ package actividad3pt3;
 
 public class Ejercicio2 {
     public static void main(String[] args) {
-        int nudos;
+        // Declararemos las variables.
+        String barco = "Programación en DAM";
+        int nudos = 20;
+        int distancia = 900;
+        double tiempoTotal;
 
+        // Calculamos el tiempo que tardará en recorer esas millas y lo guardaremos en tiempoTotal.
+        tiempoTotal = distancia / nudos;
+
+        // Imprimimos el resultado final, con el mensaje completo.
+        System.out.println("Para navegar " + distancia + " millas náuticas a " +
+                nudos + " nudos, el " + barco + " tardará " + tiempoTotal + " horas");
 
     }
 
