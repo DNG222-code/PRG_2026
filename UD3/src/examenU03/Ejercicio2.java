8eb78e5e5296101cb54b6504dbfd59f8b55a27da
DNG222-code <dnagy20474@iesjoanramis.org>
2025-10-28 15:45:01 +0100

    UD4.

---
+++ UD3/src/examenU03/Ejercicio2.java
@@ -0,0 +1,19 @@
+package examenU03;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+
+        final double arroz = 100.4;
+        double totalArroz;
+        int totalArrozEntero;
+
+        totalArroz = arroz * 8;
+        totalArrozEntero = (int) totalArroz;
+
+        System.out.println(totalArroz);
+        System.out.println(totalArrozEntero);
+
+    }
+
+}
