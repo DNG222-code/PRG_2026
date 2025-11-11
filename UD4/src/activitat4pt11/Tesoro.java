ef31a14a05b98d42c4432bcd5fd847c3c3e36633
DNG222-code <dnagy20474@iesjoanramis.org>
2025-11-11 15:40:58 +0100

    activitat4pt11.

--- UD4/src/activitat4pt11/Tesoro.java
+++ UD4/src/activitat4pt11/Tesoro.java
@@ -4,7 +4,23 @@ public class Tesoro {
 
     static void main(String[] args) {
 
-        
+        // Declaramos el array bidimensional.
+        int[][] mapa = new int[5][5];
+
+        System.out.println("Mapa de la isla solo (0): ");
+
+        // Hacemos 2 for's, 1 para la fila y el otro para las columnas.
+        for (int i = 0; i < mapa.length; i++) {
+            for (int j = 0; j < mapa[i].length; j++) {
+                mapa[i][j] = 0;
+                System.out.print(mapa[i][j] + " ");
+
+            }
+            System.out.println();
+
+        }
+
+
 
     }
 
