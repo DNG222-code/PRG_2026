7f778ca27c86301bf8a3de73891145890568d63e
DNG222-code <dnagy20474@iesjoanramis.org>
2025-12-11 14:51:26 +0100

    encapsular.

---
+++ UD6/src/encapsular/Test.java
@@ -0,0 +1,12 @@
+package encapsular;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Persona p = new Persona("Juanjo", 22);
+
+        System.out.println(p);
+    }
+
+}
