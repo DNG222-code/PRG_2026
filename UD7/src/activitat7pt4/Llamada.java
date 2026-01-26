68b62d40e097a3152bc39fc7db3648b5705508fb
DNG222-code <dnagy20474@iesjoanramis.org>
2026-01-26 14:11:53 +0100

    activitat7pt4.

---
+++ UD7/src/activitat7pt4/Llamada.java
@@ -0,0 +1,14 @@
+package activitat7pt4;
+
+import java.time.format.DateTimeFormatter;
+
+public class Llamada {
+    int numTelefono;
+    int numInterlocutor;
+    boolean esSaliente;
+    DateTimeFormatter fechaYHora;
+    enum zona {MAHON, ALAIOR, VILLACARLOS, CIUTADELLA, FERRERIES};
+    double[] tarifas = {};
+
+
+}
