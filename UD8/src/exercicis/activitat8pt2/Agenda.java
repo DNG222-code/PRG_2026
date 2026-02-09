babbb12389702cefaac19b2ccc65bfd1167ba257
DNG222-code <dnagy20474@iesjoanramis.org>
2026-02-09 20:45:43 +0100

    activitat8pt2.

--- UD8/src/exercicis/activitat8pt2/Agenda.java
+++ UD8/src/exercicis/activitat8pt2/Agenda.java
@@ -8,54 +8,73 @@ public class Agenda {
     // Declaramos los atributs.
     Scanner sc = new Scanner(System.in); // Scanner para el menú y para toda la clase.
 
+    // Declaramos variables para pedir al usuario.
+    String nombre;
+    String apellidos;
+    int telefono;
+
+
     // Declaramos los atributos para el fichero.
     File f = new File("UD8/fixers/agenda.txt");
 
     // Lectura.
     BufferedReader br = null;
-    FileReader fr = null;
 
     // Escritura.
     BufferedWriter bw = null;
-    FileWriter fw = null;
 
     public void menu() {
 
         // Declaramos la opcion.
         int opcion;
 
-        // Declaramos variables para pedir al usuario.
-        String nombre;
-        String apellidos;
-        int telefono;
-
+        // do-while hasta que presióne el número 4 para guardar y salir.
         do {
             // Mostramos el menú al usuario.
             System.out.println("----------------");
             System.out.println("----- MENÚ -----");
             System.out.println("----------------" + "\n");
+
             System.out.println("1. Nuevo contacto. ");
             System.out.println("2. Buscar nombre. ");
             System.out.println("3. Mostrar todos. ");
             System.out.println("4. Salir. " + "\n");
+
             System.out.print("Selecciona una opcion: ");
 
             opcion = sc.nextInt(); // Guardamos la opcion seleccionada.
 
             if (opcion == 1) {
-                // Pedimos al usuario los datos del nuevo contacto.
-                System.out.print("Introduce el nombre: ");
-                nombre = sc.next();
+                // Creamos una variable para saber cuantas veces va a introducir datos.
+                int veces;
 
-                System.out.print("Introduce los apellidos: ");
-                apellidos = sc.next();
+                System.out.println("Introduzca las veces que desea introducir datos: " + "\n");
+                veces = sc.nextInt();
 
-                System.out.print("Introduce el telefono: ");
-                telefono = sc.nextInt();
+                // Creamos un array para guardar los datos del contacto.
+                String[] datos;
+
+                do {
+                    // Pedimos al usuario los datos del nuevo contacto.
+                    System.out.print("Introduce el nombre: ");
+                    nombre = sc.next();
+
+                    System.out.print("Introduce los apellidos: ");
+                    apellidos = sc.next();
+
+                    System.out.print("Introduce el telefono: ");
+                    telefono = sc.nextInt();
+                    System.out.println();
+
+                    datos = new String[]{nombre, apellidos, Integer.toString(telefono)};
+
+                } while (veces >= 0 || veces <= 20);
 
                 // Llamamos al método para leer el fichero y le pasamos los atributos.
-                leerFichero(nombre, apellidos, telefono);
+                leerYEscribirFichero(datos);
+
             } else if (opcion == 2) {
+
 
             } else if (opcion == 3) {
 
@@ -65,7 +84,10 @@ public class Agenda {
         } while (opcion != 4);
     }
 
-    public void leerFichero(String nombre, String apellidos, int telefono) {
+    // Método para leer el fichero.
+    public void leerYEscribirFichero(String[] datos) {
+
+        datos = new String[]{nombre, apellidos, Integer.toString(telefono)};
 
         // Hacemos el tratamiento de error.
         try {
@@ -83,8 +105,9 @@ public class Agenda {
             // Mostramos al usuario que se háya leído bien el fichero.
             System.out.println("Se ha leído el fichero correctamente. " + "\n");
 
-            // Creamos un array para guardar los datos del contacto.
-            String[] datos = {nombre, apellidos, Integer.toString(telefono)};
+            for (String dato : datos) {
+                bw.write(dato + "\n");
+            }
 
             System.out.println("Se ha añadido un nuevo contacto al fichero. " + "\n");
 
