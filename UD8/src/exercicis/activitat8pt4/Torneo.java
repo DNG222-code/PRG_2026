013f5c4c0a4c9a65dff4974c7a7c99d32cacb32e
DNG222-code <dnagy20474@iesjoanramis.org>
2026-03-04 19:38:49 +0100

    activitat8pt4.

--- UD8/src/exercicis/activitat8pt4/Torneo.java
+++ UD8/src/exercicis/activitat8pt4/Torneo.java
@@ -22,19 +22,47 @@ public class Torneo {
 
     }
 
+    // Método para guardar los datos en un fichero Binario.
     void guardarBinario(String ruta) throws IOException {
 
+        String linea;
 
+        // Leemos el contenido del fichero.
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(ruta));
+
+        while ((linea = ois.readLine()) != null) {
+            linea = ois.readLine();
+        }
+
+        // Escribimos los datos en el fichero.
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(ruta));
+
+        while (linea != null) {
+            oos.writeObject(linea);
+        }
     }
 
+    // Método para cargar los datos de un fichero Binario.
     void cargarBinario(String ruta) throws IOException, ClassNotFoundException {
+        // Leemos todo el contenido del fichero Binario.
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(ruta));
+            int[] datos = (int[]) ois.readObject();
 
+            while ((datos = (int[]) ois.readObject()) != null) {
 
+                datos = (int[]) ois.readObject();
+            }
     }
 
     void guardarTexto(String ruta) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(ruta));
 
+        String linea = "";
 
+        while (linea != null) {
+            bw.write(linea);
+            bw.newLine();
+         }
     }
 
     // Getter & Setter.
