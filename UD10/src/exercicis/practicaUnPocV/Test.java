63a5a1342826c23f27520d446c20b4cda6128011
DNG222-code <dnagy20474@iesjoanramis.org>
2026-04-22 19:48:28 +0200

    PracticaUnPocV.

--- UD10/src/exercicis/practicaUnPocV/Test.java
+++ UD10/src/exercicis/practicaUnPocV/Test.java
@@ -1,13 +1,24 @@
 package exercicis.practicaUnPocV;
 
+import java.util.Arrays;
+
 public class Test {
 
     public static void main(String[] args) {
 
-        Double base = 0d;
-        Integer n = 0;
+        // Array de enteros.
+        Integer[] numEnteros = {1, 5, 7, 2, 10};
 
+        // Array para guardar el resultado de la raiz cuarta.
+        Double[] resultado = new Double[numEnteros.length];
 
+        // Recorremos el array de enteros y calculamos la raiz cuarta de cada uno.
+        for (int i = 0; i < numEnteros.length; i++) {
+            resultado[i] = Calculs.raizN(numEnteros[i].doubleValue(), 4);
+        }
+
+        // Mostramos el array con los resultados.
+        System.out.println(Arrays.toString(resultado));
 
     }
 }
